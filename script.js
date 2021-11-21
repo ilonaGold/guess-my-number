6878776a74e9edefa8f5b07681b0c8f1c7cacf33
ilonaGold <songa17@inbox.lv>
2021-11-22 00:09:07 +0200

    Added functionality to the reset button

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const secretNumber = Math.floor(Math.random() * 20) + 1;
+let secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -13,6 +12,7 @@ document.querySelector('.check').addEventListener('click', function() {
         // When the player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
                 
@@ -39,6 +39,20 @@ document.querySelector('.check').addEventListener('click', function() {
         }
     }
 });
+
+// Reset the game
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.floor(Math.random() * 20) + 1; 
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.body.style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
+
+
 
 
 
